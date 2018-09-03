e5ddadbe2ea241edd757cfcfcdd5af76801414da
clinton-github1 <clintonwilliams801@gmail.com>
2018-09-03 17:09:05 +0300

    module example

---
+++ Tutorial/Example/Controller/Index/Index.php
@@ -0,0 +1,51 @@
+<?php 
+
+namespace Tutorial\Example\Controller\Index;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+    * @param Context $context
+    * @param PageFactory $resultPageFactory
+    */
+    public function __construct(
+    
+    Context $context,
+        PageFactory $resultPageFactory
+    )
+    {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+    
+    public function execute()
+    {
+        $resultPageFactory = $this->resultPageFactory->create();
+        
+        // Add page Title
+        //$resultPageFactory->getConfig()->getTitle()->set(__('Today is' . date("1")));
+        
+        //Add breadcrumb
+        /** @var \Magento\Theme\Block\Html\Breadcrumbs */
+        $breadcrumbs = $resultPageFactory->getLayout()->getBlock('breadcrumbs');
+        $breadcrumbs->addCrumb('home',
+                              [
+                                  'label' => __('Home'),
+                                  'title' => __('Home'),
+                                  'link' => $this->_url->getUrl('')                             
+                              ]);
+        $breadcrumbs->addCrumb('tutorial_example',
+                              
+                              [
+                                  'label' => __('Example'),
+                                  'title' => __('Example')
+                                  
+                              ]
+                              );
+        return $resultPageFactory;
+    }
+}
+
+?>
